23a2b4393eb486343c0d585a659740cf7c61f41a
mzkmnk <sma0729mex@gmail.com>
2025-03-29 13:29:38 +0900

    feat: add drizzle config and sample schema

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import * as schema from "./schema";
+
+const client = postgres(process.env.DATABASE_URL!);
+const db = drizzle(client, { schema });
+
+export { db };
