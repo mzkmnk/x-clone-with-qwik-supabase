5d441488f7237a01af8470da4ae1d40aa4f386fe
mzkmnk <sma0729mex@gmail.com>
2025-03-30 15:56:51 +0900

    feat: dashboard route

--- src/routes/internal/dashboard/layout.tsx
+++ src/routes/internal/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div class="min-h-screen w-screen min-w-[1024px] bg-stone-900">
+    <div class="h-screen w-screen min-w-[1024px] bg-black">
       <Slot />
     </div>
   );
