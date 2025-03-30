0a6da73973499f33baf4c1a5737ff518a35ea7ef
mzkmnk <sma0729mex@gmail.com>
2025-03-30 23:01:54 +0900

    feat: update GitHub sign-in redirect and refactor form handling

--- src/routes/auth/sign-in/index.tsx
+++ src/routes/auth/sign-in/index.tsx
@@ -1,6 +1,6 @@
-import { component$, useTask$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { LuGithub } from "@qwikest/icons/lucide";
-import { routeAction$, useNavigate } from "@builder.io/qwik-city";
+import { Form, routeAction$, zod$ } from "@builder.io/qwik-city";
 import { serverSupabaseFactory } from "~/utils/supabase";
 
 export const useSignInWithGithub = routeAction$(async (_, requestEv) => {
@@ -9,7 +9,7 @@ export const useSignInWithGithub = routeAction$(async (_, requestEv) => {
   const { data, error } = await serverSupabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: `${requestEv.env.get("ROUTE_BASE_PATH")}/internal/dashboard`,
+      redirectTo: `${requestEv.url.origin}/internal/dashboard`,
     },
   });
 
@@ -20,31 +20,17 @@ export const useSignInWithGithub = routeAction$(async (_, requestEv) => {
     };
   }
 
-  return {
-    url: data.url,
-    error: null,
-  };
-});
+  throw requestEv.redirect(303, data.url);
+}, zod$({}));
 
 export default component$(() => {
   const signInWithGithub = useSignInWithGithub();
 
-  const navigate = useNavigate();
-
-  useTask$(async ({ track }) => {
-    track(() => signInWithGithub.value?.url);
-
-    if (signInWithGithub.value?.url) {
-      await navigate(signInWithGithub.value.url);
-    }
-  });
-
   return (
-    <div class="inline-flex flex-col items-center">
+    <Form class="inline-flex flex-col items-center" action={signInWithGithub}>
       <button
         class="flex gap-2 items-center border border-slate-600 rounded-md p-2 cursor-pointer hover:bg-slate-200 transition"
         type="submit"
-        onClick$={() => signInWithGithub.submit()}
       >
         <LuGithub class="text-2xl" />
         <p>Sign in with Github</p>
@@ -52,6 +38,6 @@ export default component$(() => {
       {signInWithGithub.value && signInWithGithub.value.error ? (
         <p class="text-red-500 mt-2">{signInWithGithub.value.error}</p>
       ) : null}
-    </div>
+    </Form>
   );
 });
